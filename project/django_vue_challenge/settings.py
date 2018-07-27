2a2aeb7a8509cc3fc03383803e04083869c3f95d
Vlad Elagin <vlad.elagin@keenethics.com>
2018-07-27 11:37:20 +0300

    added allowed hosts

--- project/django_vue_challenge/settings.py
+++ project/django_vue_challenge/settings.py
@@ -131,6 +131,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+if not DEBUG:
+    ALLOWED_HOSTS = ("django-vue-pomodoro.herokuapp.com",)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
